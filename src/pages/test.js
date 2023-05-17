81f59161616356afcd0ccefe4b6458c2bfb57992
Maxim Slipenko <no-reply@maxim.slipenko.com>
2023-05-17 13:53:41 +0300

    Changes

--- src/pages/test.js
+++ src/pages/test.js
@@ -4,7 +4,6 @@ import { Theme, Box } from "@quarkly/widgets";
 import { Helmet } from "react-helmet";
 import { GlobalQuarklyPageStyles } from "global-page-styles";
 import { RawHtml } from "@quarkly/components";
-import * as Components from "components";
 export default (() => {
 	return <Theme theme={theme}>
 		<GlobalQuarklyPageStyles pageUrl={"test"} />
@@ -16,17 +15,6 @@ export default (() => {
 			<link rel={"shortcut icon"} href={"https://uploads.quarkly.io/readme/cra/favicon-32x32.ico"} type={"image/x-icon"} />
 		</Helmet>
 		<Box min-width="100px" min-height="500px" background="--color-green" />
-		<Components.ImageViewer
-			showLightbox={false}
-			showButtonOpen
-			timingFunction="ease-out"
-			size="scale"
-			display="flex"
-			width="100%"
-		/>
-		<Box min-width="100px" min-height="100px" margin="0px auto 0px auto" width="50%">
-			<Components.ImageViewer showLightbox={false} showButtonOpen={false} timingFunction="ease-out" size="scale" />
-		</Box>
 		<Box min-width="100px" min-height="500px" background="--color-secondary" />
 		<RawHtml>
 			<style place={"endOfHead"} rawKey={"63d15c40f2f031001fb77bf3"}>
